3ea657b85aa02af470ca510500bea43125036cb8
pjhs2k22 <107628042+pjhs2k22@users.noreply.github.com>
2022-06-17 11:08:35 +0700

    Beta 0.1.1

--- main.js
+++ main.js
@@ -19,3 +19,19 @@ const scrosa = document.querySelector(".scrosa");
 scrosa.addEventListener("click", () => {
     window.alert("Snapchat : @chaocha06");
 });
+
+const morePhoto = document.getElementById("more-photo");
+const lessPhoto = document.getElementById("less-photo");
+const photosMore = document.querySelector(".photos-content.more");
+
+morePhoto.addEventListener("click", () => {
+    photosMore.style.display = "grid";
+    lessPhoto.style.display = "block";
+    morePhoto.style.display = "none";
+});
+
+lessPhoto.addEventListener("click", () => {
+    photosMore.style.display = "none";
+    lessPhoto.style.display = "none";
+    morePhoto.style.display = "block";
+});
